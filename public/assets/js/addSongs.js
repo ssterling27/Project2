baf53faecdaa36c357b9dac786cfc2b2c5a38820
ssterling27 <samuel.steinberg27@gmail.com>
2021-09-22 13:50:58 -0700

    fixed add songs

--- public/assets/js/addSongs.js
+++ public/assets/js/addSongs.js
@@ -1,21 +1,26 @@
 document.getElementById('searchForSong').addEventListener('click', event => {
   const search = document.getElementById('songName').value
-  axios.get(http://ws.audioscrobbler.com/2.0/?method=track.search&track=${search}&api_key=7f3036e48535565b6ab375bd9c198876&format=json)
-  .then(function (response) {
-    console.log();
-    let songs = response.data.results.trackmatches.track
+  axios.get(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${search}&api_key=7f3036e48535565b6ab375bd9c198876&format=json`)
+  .then(({ data: { results: { trackmatches: { track: songs }}}}) => {
+    console.log(songs)
     songs.forEach(song => {
-      console.log(song)
+      axios.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=7f3036e48535565b6ab375bd9c198876&artist=${song.artist}&track=${song.name}&format=json`)
+      .then(({ data: { track }}) => {
+      console.log(track)
       const songElem = document.createElement('tr')
       songElem.innerHTML = `
-      <td><img src="${song.image[1]['#text']}"></td>
-      <td>${song.name}</td>
-      <td>${song.artist}</td>
-      <td><a href="${song.url}">Link</a></td>
-      <button class="addSong">Add Song</button>
+      <td><img src="${track.album.image[1]['#text']}"></td>
+      <td>${track.name}</td>
+      <td>${track.artist.name}</td>
+      <td><a href="${track.url}">Link</a></td>
+      <td><input type="text" class="moodInput" placeholder="Mood"></td>
+      <button type="button" class="success button addSong">Add Song</button>
       `
       document.getElementById('songsHere').append(songElem)
     })
       .catch(function (error) {
         console.error(error);
-      });
+      })
+    })
+  })
+})
