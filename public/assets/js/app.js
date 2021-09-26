08dd14d9e64f7de1ee1c6b29a40cd3d3808d5cd4
ssterling27 <samuel.steinberg27@gmail.com>
2021-09-26 05:17:08 -0700

    homepage styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,5 @@
 const { axios } = window
+let i = 0
 
 axios.get('/api/playlists/public', {
   headers: {
@@ -10,28 +11,47 @@ axios.get('/api/playlists/public', {
     playlists.forEach(({ id, name, u: { username }}) => {
       axios.get(`../api/playlists/${id}/song`)
       .then(({data: song }) => {
+      if (i < 5) {
+        const carouselElem = document.createElement('div')
+      carouselElem.classList = "card carouselThing"
+      carouselElem.src = `${song.artwork}`
+      carouselElem.innerHTML = `
+      <img src="${song.artwork}">
+      <div class="card-section cut">
+        <h5 class="header">${name}</h5>
+        <div class="userButton">
+        <h6 class="badge bg-primary rounded-pill">${username}</h6>
+        <a href="/playlists/${id}" class="button">View Playlist</a>
+        </div>
+      </div>
+      `
+        document.getElementById(`carouselItem${i}Here`).append(carouselElem)
+        
+      }
       const playlistElem = document.createElement('div')
-      playlistElem.classList = 'cell small-4'
+      playlistElem.classList = 'cell small-3'
+      playlistElem.style = 'margin-bottom: 10px;'
       playlistElem.id = `${id}`
       // playlistElem.style = ''
       playlistElem.innerHTML = `
-      <div class="card cardStyling">
+      <div class="card space cardStyling">
       <img class="playlistLink" src="${song.artwork}">
-      <div class="card-section playlistLink">
-        <h5 class="playlistLink">${name}</h5>
+      <div class="card-section cut playlistLink bottom">
+        <h5 class="playlistLink header" style="font-size: 1.5rem;">${name}</h5>
         <h6 class="badge bg-primary rounded-pill playlistLink">${username}</h6>
       </div>
       </div>
       `
       document.getElementById('PlaylistHere').append(playlistElem)
+      i++
       })
     })
-  })
-
+    })
 .catch (err => {
   console.log(err)
   window.location = '/login'
 })
+
 
 document.addEventListener('click', event => {
   if (event.target.classList.contains('playlistLink')) {
@@ -44,3 +64,19 @@ document.getElementById('logOut').addEventListener('click', () => {
   localStorage.removeItem('token')
   window.location = '/login'
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  var options = {
+    padding: 200,
+  }
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+  window.next = function () {
+    var el = document.querySelector(".carousel");
+    var l = M.Carousel.getInstance(el);
+    l.next(1);
+  }
+  setInterval(() => next(), 2000)
+});
+
+// new Sign().print(document.body)
